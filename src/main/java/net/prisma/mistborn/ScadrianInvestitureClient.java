e22e45d1003746d8531185850a29f35b54a82a47
Prismatica <prismaticairis@gmail.com>
2024-07-11 22:57:20 -0400

    Initial commit?

---
+++ src/main/java/net/prisma/mistborn/ScadrianInvestitureClient.java
@@ -0,0 +1,10 @@
+package net.prisma.mistborn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScadrianInvestitureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/prisma/mistborn/ScadrianInvestitureDataGenerator.java
@@ -0,0 +1,11 @@
+package net.prisma.mistborn;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ScadrianInvestitureDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
